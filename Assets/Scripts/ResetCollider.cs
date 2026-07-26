3771d7743c039b8a9937862a5dfccb24ab88496f
Chris DeLeon <chris@gamkedo.com>
2026-07-26 16:52:56 -0500

    blockout for hotel-styled museum exterior

--- Assets/Scripts/ResetCollider.cs
+++ Assets/Scripts/ResetCollider.cs
@@ -6,8 +6,15 @@ public class ResetCollider : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("reset collider trigger from " + gameObject.name + " touched by " + other.name);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        WalkControl wcScript = other.GetComponentInParent<WalkControl>();
+        if(wcScript) // should only react to player
+        {
+            Debug.Log("reset collider trigger from " + gameObject.name + " touched by " + other.name);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);    
+        } else
+        {
+            Debug.Log("non-player fell from stage " + gameObject.name + " look for: " + other.name);
+        }
 
     }
 }
