2a56e9ab00382727d302c81a5e8a1bf804852153
Adeel Bashir <99957815+AdeelGameDev@users.noreply.github.com>
2026-08-16 17:07:14 +0500

    ResetCollider works with the new controller

--- Assets/Scripts/ResetCollider.cs
+++ Assets/Scripts/ResetCollider.cs
@@ -6,8 +6,8 @@ public class ResetCollider : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        WalkControl wcScript = other.GetComponentInParent<WalkControl>();
-        if(wcScript) // should only react to player
+        PlayerMovement playerMovement = other.GetComponentInParent<PlayerMovement>();
+        if(playerMovement) // should only react to player
         {
             Debug.Log("reset collider trigger from " + gameObject.name + " touched by " + other.name);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);    
