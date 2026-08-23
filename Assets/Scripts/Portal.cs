e716e9edb0eab2ed9da5a58202b190062e746a65
Adeel Bashir <99957815+AdeelGameDev@users.noreply.github.com>
2026-08-23 20:36:04 +0500

    click to regain focus does not open link

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class Orb : MonoBehaviour , IInteractable
 {
+    private bool allowInteraction = true; 
     [SerializeField] private GameObject highlightObject;
 
     [SerializeField] private string sceneOrURL;
@@ -16,15 +17,23 @@ public class Orb : MonoBehaviour , IInteractable
     public void Clear()
     {
         highlightObject.gameObject.SetActive(false);
+        allowInteraction = true;
     }
 
     public void Highlight()
     {
+        if(allowInteraction)
         highlightObject.gameObject.SetActive(true);
     }
 
     public void Interact()
     {
+
+        if (!allowInteraction)
+            return;
+
+        Clear();
+        allowInteraction = false;
         if(sceneOrURL.Contains("https"))
         {
             Debug.Log("opening URL: " + sceneOrURL);
